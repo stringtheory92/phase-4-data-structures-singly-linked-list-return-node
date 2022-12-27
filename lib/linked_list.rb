77f952d0acecec0c114de5bc5d898dda8e4db858
Adam Kabak <adamkabak@yahoo.com>
2022-12-27 05:06:49 -0500

    commit tree 706d7a6e1f8e18b8f78ceb9b776f5233ca367943

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,39 @@ class LinkedList
 
   def nth_from_end(n)
     # your code here
+
+    # iterate to the end of the list and then reverse n places
+    if !head 
+      return nil
+    elsif n < 1
+      return nil
+    end
+
+    # initialize a counter for list length
+    length = 1
+    last_node = self.head
+    while last_node.next_node
+      last_node = last_node.next_node
+      length += 1
+    end
+    # we now have the length of the list and the last node
+    # if n is greater than the length of the list, return nil
+    if n > length 
+      return nil
+    end
+
+    response = self.head
+    # iterate over the list again, checking to see if there is a node n places away from the end. 
+    (length - (n)).times do 
+      response = response.next_node
+    end
+
+    response.value
+    # if there is, response = current node. 
+
+    # if not, return response
+
+# 1 -> 2 -> 3
   end
 
 end
